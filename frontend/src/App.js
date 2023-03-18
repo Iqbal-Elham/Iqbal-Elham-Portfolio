9857256344a4753d84d7cd6dc45828efb4253d75
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-18 17:48:26 +0430

    Initialization of components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import React from 'react';
+import './App.scss';
+import About from './container/About/About';
+import Footer from './container/Footer/Footer';
+import Header from './container/Header/Header';
+import Skills from './container/Skills/Skills';
+import Testimonial from './container/Testimonial/Testimonial';
+import Work from './container/Work/Work';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div>
-      hello iqbal
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
