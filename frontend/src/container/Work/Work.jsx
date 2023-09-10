b2ff2e7e2b1c8c0d6dd8c3391c89a0615873a453
iqbal-elham <iqbal.elham7@gmail.com>
2023-09-10 19:28:18 +0430

    Fix the heading hierarchy

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -106,7 +106,7 @@ const Work = () => {
             </div>
 
             <div className="app__work-content app__flex">
-              <h4 className="bold-text">{work.title}</h4>
+              <h3 className="bold-text">{work.title}</h3>
               <p className="p-text" style={{ marginTop: 10 }}>
                 {work.description}
               </p>
