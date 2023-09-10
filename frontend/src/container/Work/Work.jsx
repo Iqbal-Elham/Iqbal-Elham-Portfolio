f99158cbd27513333a8196ad59a56c08878de7c9
Iqbal Elham <iqbal.elham7@gmail.com>
2023-09-10 15:27:46 +0430

    Update Work.jsx

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -82,7 +82,7 @@ const Work = () => {
                 }}
                 className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                <a href={work.projectLink} target="_blank" rel="noreferrer" aria-label="demo">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -92,7 +92,7 @@ const Work = () => {
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                <a href={work.codeLink} target="_blank" rel="noreferrer" arial-label="source">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -112,8 +112,8 @@ const Work = () => {
               </p>
 
               <div className="app__work-btn">
-                <a href={work.projectLink} target="_blank" rel="noreferrer">Demo</a>
-                <a href={work.codeLink} target="_blank" rel="noreferrer">Source</a>
+                <a href={work.projectLink} target="_blank" rel="noreferrer" aria-label="demo">Demo</a>
+                <a href={work.codeLink} target="_blank" rel="noreferrer" arial-label="source">Source</a>
                </div>
 
               <div className="app__work-tag app__flex">
