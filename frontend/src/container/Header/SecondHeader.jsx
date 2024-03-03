dc14efac07d18b4987b1ecf4e552cebc5a7c3730
iqbal-elham <iqbal.elham7@gmail.com>
2024-03-03 11:40:54 +0430

    Add the second header jsx file

--- frontend/src/container/Header/SecondHeader.jsx
+++ frontend/src/container/Header/SecondHeader.jsx
@@ -5,8 +5,8 @@ import Particles, { initParticlesEngine } from "@tsparticles/react";
 import AppWrap from "../../wrapper/AppWrap";
 
 // import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-// import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 // import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
 const SecondHeader = () => {
@@ -19,8 +19,8 @@ const SecondHeader = () => {
       // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
       // starting from v2 you can add only the features you need reducing the bundle size
       //await loadAll(engine);
-      await loadFull(engine);
-      //   await loadSlim(engine);
+      //   await loadFull(engine);
+      await loadSlim(engine);
       //await loadBasic(engine);
     }).then(() => {
       setInit(true);
@@ -95,6 +95,8 @@ const SecondHeader = () => {
         },
         size: {
           value: { min: 1, max: 5 },
+          height: 1052,
+          width: 1911 
         },
       },
       detectRetina: true,
